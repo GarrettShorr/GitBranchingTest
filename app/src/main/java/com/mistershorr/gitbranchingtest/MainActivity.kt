b65dd8cf9be94ff9a70dc897b412196e447e493f
Garrett Shorr <g@garrettshorr.com>
2021-04-28 09:28:58 -0700

    feature1 branch new comment

--- app/src/main/java/com/mistershorr/gitbranchingtest/MainActivity.kt
+++ app/src/main/java/com/mistershorr/gitbranchingtest/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // look at me! i'm a comment for a new feature why not
     }
 }
